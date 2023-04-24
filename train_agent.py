fc78a31d1052d8b9f4399ccbd337e4efaf851b6a
Hongyi Zheng <zhenghongyi2000@hotmail.com>
2023-04-24 13:19:42 -0400

    enable parallelism

--- train_agent.py
+++ train_agent.py
@@ -1,28 +1,25 @@
 import gym
 import disneyenv
 
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.callbacks import EvalCallback, CallbackList, ProgressBarCallback
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
+from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3 import PPO
 
 
 def get_train_env():
     train_env = gym.make("disneyenv/Disney-v0", train=True)
-    train_env = Monitor(train_env, filename="./monitor_logs/train",
-                        info_keywords=("current_date",))
     return train_env
 
 
 def get_eval_env():
     eval_env = gym.make("disneyenv/Disney-v0", train=False)
-    eval_env = Monitor(eval_env, filename="./monitor_logs/eval",
-                       info_keywords=("current_date",))
     return eval_env
 
 
-train_env = DummyVecEnv([get_train_env])
-eval_env = DummyVecEnv([get_eval_env])
+train_env = VecMonitor(SubprocVecEnv([lambda: get_train_env()
+                                      for i in range(32)], start_method="fork"), filename="./monitor_logs/train", info_keywords=("current_date",))
+eval_env = VecMonitor(SubprocVecEnv([lambda: get_eval_env()
+                                     for i in range(32)], start_method="fork"), filename="./monitor_logs/eval", info_keywords=("current_date",))
 
 model = PPO("MultiInputPolicy", train_env, verbose=1, device="cpu")
 
@@ -30,7 +27,4 @@ model = PPO("MultiInputPolicy", train_env, verbose=1, device="cpu")
 eval_callback = EvalCallback(eval_env, best_model_save_path="./eval_results/",
                              log_path="./eval_results/", eval_freq=5000, n_eval_episodes=15)
 
-pb_callback = ProgressBarCallback()
-
-model.learn(total_timesteps=100000, callback=CallbackList(
-    [eval_callback, pb_callback]))
+model.learn(total_timesteps=1000000, callback=eval_callback)
